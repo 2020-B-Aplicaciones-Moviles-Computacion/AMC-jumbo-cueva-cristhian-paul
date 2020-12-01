62fc8cc2f33e56bef5b7f7ad83f4d86f7bb98889
Cristhian Jumbo <sonclonmenax@gmail.com>
2020-12-01 16:49:48 -0500

    ciclo de vida de una actividad android

--- app/src/main/java/com/example/movilescomputacion/MainActivity.kt
+++ app/src/main/java/com/example/movilescomputacion/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.movilescomputacion
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button_ir_ciclo_vida)
+        button.setOnClickListener {
+            irCicliVida()
+        }
+
+    }
+
+    fun irCicliVida() {
+        val intentExplicito = Intent(
+            this,
+            ACicloVida::class.java
+        )
+        startActivity(intentExplicito)
     }
 }
