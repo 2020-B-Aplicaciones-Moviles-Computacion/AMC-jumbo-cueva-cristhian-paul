e271b892e33726b3d16c2a14e5f3738dec59dd24
Cristhian Jumbo <sonclonmenax@gmail.com>
2020-12-09 18:50:16 -0500

    update list build context menu

--- app/src/main/java/com/example/movilescomputacion/BlistView.kt
+++ app/src/main/java/com/example/movilescomputacion/BlistView.kt
@@ -1,7 +1,9 @@
 package com.example.movilescomputacion
 
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
@@ -21,6 +23,39 @@ class BlistView : AppCompatActivity() {
 
         val listView = findViewById<ListView>(R.id.lv_entrenador)
         listView.adapter = adaptador
+
+        listView
+                .setOnItemLongClickListener { parent, view, position, id ->
+                    Log.i("intent-explicito", "Hola ${position} ${id}")
+                    val builder = AlertDialog
+                            .Builder(this)
+                    builder.setMessage("hola")
+                    val seleccionUsuario = booleanArrayOf(
+                            true,
+                            false,
+                            false
+                    )
+                    val opcioes = resources.getStringArray(R.array.string_arrays)
+                    builder.setMultiChoiceItems(
+                            opcioes,
+                            seleccionUsuario,
+                            null
+                    )
+                    builder.setMessage("hola")
+                            .setPositiveButton(
+                                    "Si",
+                                    {dialog, which ->
+                                        Log.i("intent-explicito", "Si")
+                                    }
+                            )
+                            .setNegativeButton(
+                                    "Si",
+                                    null
+                            )
+                    val dialogo = builder.create()
+                    dialogo.show()
+                    return@setOnItemLongClickListener true
+                }
 
         adaptador.notifyDataSetChanged()
 
--- app/src/main/java/com/example/movilescomputacion/MainActivity.kt
+++ app/src/main/java/com/example/movilescomputacion/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.movilescomputacion
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
@@ -15,27 +17,71 @@ class MainActivity : AppCompatActivity() {
 
         val button2 = findViewById<Button>(R.id.btn_ir_list_view)
         button2.setOnClickListener {
-            irListView()
+            irActivida(BlistView::class.java)
         }
 
         val button = findViewById<Button>(R.id.button_ir_ciclo_vida)
         button.setOnClickListener {
-            irCicliVida()
+            irActivida( ACicloVida::class.java)
+        }
+
+        val buttonIntentExplicitoParametros = findViewById<Button>(R.id.btn_ir_intent_explicito_parametros)
+        buttonIntentExplicitoParametros.setOnClickListener {
+
+            val intentExplicito = Intent(
+                    this,
+                    CIntentExplicitoParametros::class.java
+            )
+            intentExplicito.putExtra("nombre", "Cristhian")
+            intentExplicito.putExtra("apellido", "Jumbo")
+            intentExplicito.putExtra("edad", 31)
+            startActivityForResult(intentExplicito, 102)
+//            val parametros = arrayListOf<Pair<String, *>>(
+//                Pair("nombre", "Cristhian"),
+//                Pair("apellido", "Jumbo"),
+//                Pair("edad", 23)
+//            )
+//            irActivida(CIntentExplicitoParametros::class.java, parametros)
         }
     }
 
-    fun irCicliVida() {
+    fun irActivida(
+        clase: Class<*>,
+        parametros: ArrayList<Pair<String, *>>? = null
+    ) {
         val intentExplicito = Intent(
             this,
-            ACicloVida::class.java
+            clase
         )
+        if (parametros != null){
+            parametros.forEach{
+                var nombreVariables = it.first
+                var valorVariable =  it.second is Any
+                intentExplicito.putExtra(nombreVariables, valorVariable)
+            }
+        }
+        intentExplicito.putExtra("nombre", "Adrian")
         startActivity(intentExplicito)
     }
-    fun irListView() {
-        val intentExplicito = Intent(
-            this,
-            BlistView::class.java
-        )
-        startActivity(intentExplicito)
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when(requestCode){
+           102 -> {
+               if(requestCode == Activity.RESULT_OK) {
+                   Log.i("intent-explicito", "Si actulizo los datos")
+                   if(data != null) {
+                       val nombre = data.getStringExtra("nombre")
+                       val edad = data.getIntExtra("edad", 0)
+
+                       Log.i("intent-explicito", "Nombre: ${nombre} Edad: ${edad}")
+                   }
+
+               } else {
+                   Log.i("intent-explicito", "Usuario no llego los datos")
+               }
+           }
+        }
     }
 }
