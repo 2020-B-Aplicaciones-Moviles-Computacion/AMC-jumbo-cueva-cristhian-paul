51ec6f10866b6d0167686b791fcf084370b514fd
Cristhian Jumbo <sonclonmenax@gmail.com>
2021-01-03 19:55:58 -0500

    deber de metodos crud klaxon

--- app/src/main/java/com/example/movilescomputacion/HHttpActivity.kt
+++ app/src/main/java/com/example/movilescomputacion/HHttpActivity.kt
@@ -2,68 +2,189 @@ package com.example.movilescomputacion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.ResultReceiver
 import android.util.Log
 import com.beust.klaxon.Klaxon
-import com.github.kittinunf.fuel.httpGet
-import com.github.kittinunf.fuel.httpPost
+import com.github.kittinunf.fuel.*
 import com.github.kittinunf.result.Result
 class HHttpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_h_http)
-        //metodoGet()
-        metodoPost()
+
+        //  metodoGetAll()
+        // metodoGet()
+        //  metodoPost()
+        // metodoUpdate(1)
+        // metodoDelete(1)
+        metodoBusquedaUserId(3)
     }
+
+//    fun metodoGetAll() {
+//        "https://jsonplaceholder.typicode.com/posts/"
+//            .httpGet()
+//            .responseString{
+//                    request, response, result ->
+//                when (result) {
+//                    is Result.Failure -> {
+//                        val error = result.getException()
+//                        Log.i("http-klaxon", "Error :${error}")
+//                    }
+//                    is Result.Success -> {
+//                        val postString = result.get()
+//                        Log.i("http-klaxon", "Error :${postString}")
+//                        val arreglo = Klaxon()
+//                            .parseArray<IPostHttp>(postString)
+//                        if (arreglo != null) {
+//                            arreglo
+//                                .forEach{
+//                                    Log.i("http-klaxon", "Error :${it?.title}")
+//                                }
+//                        }
+//
+//                    }
+//                }
+//            }
+//    }
 
     fun metodoGet() {
-        "https://jsonplaceholder.typicode.com/posts/"
-            .httpGet()
-            .responseString{
-                    request, response, result ->
-                when (result) {
-                    is Result.Failure -> {
-                        val error = result.getException()
-                        Log.i("http-klaxon", "Error :${error}")
-                    }
-                    is Result.Success -> {
-                        val postString = result.get()
-                        Log.i("http-klaxon", "Error :${postString}")
-                        val arreglo = Klaxon()
-                            .parseArray<IPostHttp>(postString)
-                        if (arreglo != null) {
-                            arreglo
-                                .forEach{
-                                    Log.i("http-klaxon", "Error :${it?.title}")
-                                }
+        "https://jsonplaceholder.typicode.com/posts/1"
+                .httpGet()
+                .responseString { req, res, result ->
+                    when (result) {
+                        is Result.Failure -> {
+                            val error = result.getException()
+                            Log.i("http-klaxon", "Error: ${error}")
                         }
+                        is Result.Success -> {
+                            val postString = result.get()
+                            val post = Klaxon()
+                                    .parse<IPostHttp>(postString)
+                            Log.i("http-klaxon", "Titulo del post:  ${post?.title}")
+                        }
+                    }
+                }
+    }
 
-                    }
-                }
-            }
-    }
     fun metodoPost() {
-        val parametros: List<Pair<String, *>> = listOf(
-            "title" to "titullo Moviles",
-            "body" to "descripcion moviles",
-            "userId" to 1
+        val parametros: List<Pair<String, String>> = listOf(
+                "title" to "titulo moviles",
+                "body" to "descripcion moviles",
+                "userId" to "1"
         )
-        "https://jsonplaceholder.typicode.com/posts/"
-            .httpPost(parametros)
-            .responseString{
-                    request, response, result ->
-                when (result) {
-                    is Result.Failure -> {
-                        val error = result.getException()
-                        Log.i("http-klaxon", "Error :${error}")
-                    }
-                    is Result.Success -> {
-                        val postString = result.get()
-                        Log.i("http-klaxon", "Error :${postString}")
-                        val post = Klaxon()
-                            .parseArray<IPostHttp>(postString)
-                        Log.i("http-klaxon", "Error :${post}")
+
+        "https://jsonplaceholder.typicode.com/posts"
+                .httpPost(parametros)
+                .responseString { req, res, result ->
+                    when (result) {
+                        is Result.Failure -> {
+                            val error = result.getException()
+                            Log.i("http-klaxon", "Error: ${error}")
+                        }
+                        is Result.Success -> {
+                            val postString = result.get()
+                            val post = Klaxon()
+                                    .parse<IPostHttp>(postString)
+                            Log.i("http-klaxon", "${post?.title}")
+                        }
                     }
                 }
-            }
     }
+
+    fun  metodoUpdate(
+            id: Int
+    ) {
+
+        val parametros: List<Pair<String, String>> = listOf(
+                "title" to "Titulo moviles version actualiado",
+                "body" to "descripcion moviles version actualizado",
+                "userId" to "1"
+        )
+        "https://jsonplaceholder.typicode.com/posts/${id}"
+                .httpPut(parametros)
+                .responseString { req, res, result ->
+                    when (result) {
+                        is Result.Failure -> {
+                            val error = result.getException()
+                            Log.i("http-klaxon", "Error ${error}")
+                        }
+                        is Result.Success -> {
+                            /*val usuarioString = result.get()
+                            Log.i("http-klaxon", "${usuarioString}")*/
+                            val postString = result.get()
+                            Log.i("http-klaxon", "Post Actualizado:${postString}")
+                            val posts = Klaxon()
+                                    .parse<IPostHttp>(postString)
+                            Log.i("http-klaxon", "titulo  de l post actualizado: ${posts?.title}")
+                        }
+                    }
+                }
+    }
+
+    fun metodoDelete(
+            id:Int
+    ) {
+        "https://jsonplaceholder.typicode.com/posts/${id}"
+                .httpDelete()
+                .responseString { req, res, result ->
+                    when (result) {
+                        is Result.Failure -> {
+                            val error = result.getException()
+                            Log.i("http-klaxon", "Error: ${error}")
+                        }
+                        is Result.Success -> {
+                            val postString = result.get()
+                            Log.i("http-klaxon", "${postString}")
+                        }
+                    }
+                }
+    }
+
+    fun metodoBusquedaUserId(
+            userId: Int
+    ) {
+        "https://jsonplaceholder.typicode.com/posts?userId=${userId}"
+                .httpGet()
+                .responseString{ req, res, result ->
+                    when(result){
+                        is Result.Failure -> {
+                            val error = result.getException()
+                            Log.i("http-klaxon", "Error ${error}")
+                        }
+                        is Result.Success -> {
+                            val postString = result.get()
+                            Log.i("http-klaxon", "${postString}")
+                            val arregloPosts = Klaxon()
+                                    .parseArray<IPostHttp>(postString)
+
+                        }
+                    }
+                }
+    }
+
+    /* fun metodoPost() {
+         val parametros: List<Pair<String, *>> = listOf(
+             "title" to "titullo Moviles",
+             "body" to "descripcion moviles",
+             "userId" to 1
+         )
+         "https://jsonplaceholder.typicode.com/posts/"
+             .httpPost(parametros)
+             .responseString{
+                     request, response, result ->
+                 when (result) {
+                     is Result.Failure -> {
+                         val error = result.getException()
+                         Log.i("http-klaxon", "Error :${error}")
+                     }
+                     is Result.Success -> {
+                         val postString = result.get()
+                         Log.i("http-klaxon", "Error :${postString}")
+                         val post = Klaxon()
+                             .parseArray<IPostHttp>(postString)
+                         Log.i("http-klaxon", "Error :${post}")
+                     }
+                 }
+             }
+     }*/
 }
--- app/src/main/java/com/example/movilescomputacion/IPostHttp.kt
+++ app/src/main/java/com/example/movilescomputacion/IPostHttp.kt
@@ -1,19 +1,19 @@
 package com.example.movilescomputacion
 
 class IPostHttp (
-    val id: Int,
-    val userId: Any,
-    val title: String,
-    val body: String
+    val id: Int?,
+    var userId: Any?,
+    val title: String?,
+    val body: String?
 ) {
 
-    var userIdTranformado: Int = 0;
+   // var userIdTranformado: Int = 0;
     init {
         if(userId is String){
-            userIdTranformado = (userId as String).toInt()
+            userId = (userId as String).toInt()
         }
         if(userId is Int){
-            userIdTranformado = userId.toInt()
+            userId = userId
         }
 
     }
